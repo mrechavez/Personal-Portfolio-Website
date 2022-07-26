12d42dc32ab0e187e5a72c3bab601643fb032aa4
mrechavez <milesechavez@gmail.com>
2022-07-26 18:00:48 -0400

    added JS show and hide menus

---
+++ main.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+//show menu
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+//hide menu
+closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
